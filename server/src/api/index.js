cef88ef4e9426e0d11e217e38304157468a4f31e
PeterWarrington <sothisispeter@gmail.com>
2020-03-13 20:43:30 +0000

    Add renderer modulisation

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -5,29 +5,9 @@ const port = 80
 app.set('view engine', 'ejs'); // Load pug layout engine
 app.set('views', process.cwd() + '/res/templates/'); // Set location of template files
 
-app.get('/*', (req, res) => {
-    res.render('homepage', {
-        basics: {
-            name: 'Project #Q', 
-            Objectname: '#Q'
-        }, 
-        user: {
-            subjects: [
-                {
-                    name: "Subject"
-                }
-        ]},
-        posts: {
-            popular: [
-                {
-                    title: "Title",
-                    userName: "UserName",
-                    simpleDate: "Time ago",
-                    descriptionShort: "Shortened description"
-                }
-            ]
-        }
-    });
+console.log(process.cwd() + "/src/api/renderers/homepage.js");
+app.get('/', (req,res) => {
+    require(process.cwd() + '/src/api/renderers/homepage.js').main(req,res);
 });
 
 app.listen(port, () => console.log(`Project #Q server has started on port ${port}!`));
---
+++ server/src/api/renderers/homepage.js
@@ -0,0 +1,49 @@
+exports.main = (req,res) => {
+    res.render('homepage', {
+        basics: {
+            name: 'Project #Q', 
+            Objectname: '#Q'
+        }, 
+        user: {
+            subjects: [
+                {
+                    name: "Subject"
+                }
+        ]},
+        posts: {
+            popular: [
+                {
+                    title: "Title",
+                    userName: "UserName",
+                    simpleDate: "Time ago",
+                    descriptionShort: "Shortened description"
+                },
+                {
+                    title: "Title",
+                    userName: "UserName",
+                    simpleDate: "Time ago",
+                    descriptionShort: "Shortened description"
+                },
+                {
+                    title: "Title",
+                    userName: "UserName",
+                    simpleDate: "Time ago",
+                    descriptionShort: "Shortened description"
+                },
+                {
+                    title: "Title",
+                    userName: "UserName",
+                    simpleDate: "Time ago",
+                    descriptionShort: "Shortened description"
+                },
+                {
+                    title: "Title",
+                    userName: "UserName",
+                    simpleDate: "Time ago",
+                    descriptionShort: "Shortened description"
+                },
+
+            ]
+        }
+    });
+}

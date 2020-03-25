c69577bb02e3b90a6515e5936097bbfbbb725699
PeterWarrington <sothisispeter@gmail.com>
2020-03-25 17:39:53 +0000

    Get most popular posts based on view count table BEWARE: This is using some really weird SQL query that I hacked together that I don't even understand how works

--- server/src/api/mysqlQueryer.js
+++ server/src/api/mysqlQueryer.js
@@ -1,19 +1,24 @@
 const mysql = require("mysql");
 const config = require(process.cwd() + "/config.js").config;
 
-function generateConnectionForReading(table, callback) {
+function generateConnectionForReading(type, callback) {
     var con = mysql.createConnection({
         host: config.databaseServer,
-        user: "read_" + table,
-        password: config.databasePass + "read_" + table
+        user: "read_" + type,
+        password: config.databasePass + "read_" + type,
+        database: "project-q"
     });
     callback(con);
 }
 
 exports.getPopularPosts = async (limit, callback) => {
-    generateConnectionForReading("objects", (con) => {
-        sqlQuery = "SELECT `object-post-date`, `object-title`, `object-description`, `object-creator-user`, `object-file-extension`, `object-tags` FROM `project-q`.objects ORDER BY `object-access-count` DESC LIMIT " + limit.toString() + ";";
-        console.log(sqlQuery);
+    generateConnectionForReading("public", (con) => {
+        sqlQuery = "\
+        SELECT `object-post-date`, `object-title`, `object-description`, `object-creator-user`, `object-file-extension`, `object-tags` \
+        FROM (SELECT * FROM `project-q`.view_count LIMIT 500) AS InnerTable \
+        INNER JOIN objects ON objects.`object-hash-id`=`view-object-hash-id` \
+        GROUP BY `view-object-hash-id` ORDER BY COUNT(`view-object-hash-id`) DESC LIMIT 5 \
+        "; // I don't even understand how this query works, but it *should* get the most reguarly accessed posts out of the previous 500 accesses
         con.query(sqlQuery, function (err, result) {
             if (err) throw err;
             if (result != null) {

a64e8963dcc053fe3e97b1607e9b351093570e61
PeterWarrington <sothisispeter@gmail.com>
2020-08-14 18:32:35 +0100

    Add review inputs

--- res/static/res/post_detail/script.js
+++ res/static/res/post_detail/script.js
@@ -28,9 +28,82 @@ function loadMoreReviews() {
 }
 
 window.addEventListener("load", async (event) => {
-    document.getElementById("more-review-btn").addEventListener('click', function(event) {
-        loadMoreReviews();
-    });
+    try {
+        document.getElementById("more-review-btn").addEventListener('click', function(event) {
+            loadMoreReviews();
+        });
+    } catch {};
+
+    // Enable textarea auto-expand for review
+    textarea = document.getElementById("review-text");
+    var heightLimit = 250;
+    textarea.oninput = function() {
+        textarea.style.height = ""; /* Reset the height*/
+        textarea.style.height = Math.min(textarea.scrollHeight, heightLimit) + "px";
+    };
+      
+    // Handle star rating selection and effects
+    inputStarBtns = document.getElementsByClassName("input-star-btn");
+    for (i=0; i<inputStarBtns.length; i++) {
+        starBtnOuter = inputStarBtns[i];
+
+        starBtnOuter.onmouseover =  function(event) {
+            if (event.srcElement.id == "") { // mousing over the icon inside the button
+                event.srcElement.id = event.srcElement.parentElement.id;
+            }
+
+            starBtnNumberOuter = parseInt(event.srcElement.id.slice(-1)); // e.g "input-star-btn-1" -> "1" -> 1
+
+            for (i=0; i<inputStarBtns.length; i++) {
+                starBtnInner = inputStarBtns[i];
+                starBtnNumberInner = parseInt(starBtnInner.id.slice(-1));
+
+                if (starBtnNumberInner <= starBtnNumberOuter) {
+                    if (!starBtnInner.classList.contains("active")) {
+                        starBtnInner.classList.add("active");
+                    }
+                }
+            };
+        };
+
+        starBtnOuter.onmouseout = function(event) {
+            if (event.srcElement.id == "") { // mousing over the icon inside the button
+                event.srcElement.id = event.srcElement.parentElement.id;
+            }
+
+            starBtnNumberOuter = parseInt(event.srcElement.id.slice(-1));
+
+            for (i=0; i<inputStarBtns.length; i++) {
+                starBtnInner = inputStarBtns[i];
+                starBtnNumberInner = parseInt(starBtnInner.id.slice(-1));
+
+                if (starBtnNumberInner <= starBtnNumberOuter) {
+                    if (starBtnInner.classList.contains("active")) {
+                        starBtnInner.classList.remove("active");
+                    }
+                }
+            };
+        };
+
+        starBtnOuter.addEventListener('click', function(event) {
+            for (i=0; i<inputStarBtns.length; i++) {
+                starBtnInner = inputStarBtns[i];
+                starBtnNumberClicked = parseInt(event.srcElement.id.slice(-1));
+
+                starBtnInner.onmouseout = function(event) {
+                    for (x=0; x<inputStarBtns.length; x++) {
+                        starBtnNumberInner = parseInt(inputStarBtns[x].id.slice(-1));
+
+                        if (starBtnNumberInner > starBtnNumberClicked) {
+                            if (inputStarBtns[x].classList.contains("active")) {
+                                inputStarBtns[x].classList.remove("active");
+                            }
+                        }
+                    }
+                };
+            }
+        });
+    };
 
     window.onscroll(null);
 });

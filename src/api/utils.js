435e383de2fab890601dae1e05a27f24fdb12d0e
PeterWarrington <sothisispeter@gmail.com>
2020-08-25 19:57:28 +0100

    Add util to check if object with id already exists

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,3 +1,5 @@
+const mysqlQueryer = require(process.cwd() + "/src/api/mysqlQueryer.js");
+
 exports.send404 = (req, res) => {
     res.status(404);
     res.render('error', {
@@ -34,3 +36,19 @@ exports.sendNonHtmlOtherError = (req, res, httpCode, errorCode) => {
     res.status(httpCode);
     res.end('{"success": "false", "errorStatusCode": ' + httpCode.toString() +', "internalErrorCode": "' + errorCode + '"}');
 };
+
+// check if an object exists
+exports.ifObjectExists = (objectId, falseFunc, trueFunc) => {
+    mysqlQueryer.generateDbConnection("read", "public", (con) => {
+        sqlQuery = "SELECT `object-hash-id` FROM `objects` WHERE `object-hash-id`=" + con.escape(objectId);
+
+        con.query(sqlQuery, function (err, results) {
+            if (err) throw err;
+
+            if (results.length >= 1)
+                trueFunc()
+            else
+                falseFunc();
+        });
+    });
+}
